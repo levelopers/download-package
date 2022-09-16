5dc5ad8833d88c8a1132f2f274cdbf70baab39ad
Zack Yin <youcancallmeallen@gmail.com>
2022-09-17 03:40:35 +0800

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,11 +1,11 @@
 
 $Source_file = "./package-lock.json"
-$Dest_folder = "./packs"
 $Reg1 = [regex]'resolved.*'
 $Reg2 = [regex]'https?:\/\/.*\.\w+'
 $Reg3 = [regex]'([^/]+)?\.(\w+)$'
 
 [System.Collections.ArrayList]$Lines = @()
+new-item "./packs" -Type Directory
 
 foreach ($line in Get-Content $Source_file) {
 
@@ -19,7 +19,7 @@ foreach ($line in Get-Content $Source_file) {
 foreach ($url in $Lines) {
     $File_name = $url | Select-String $Reg3 -AllMatches | ForEach-Object { $_.Matches.Value }
     # echo $File_name
-    Invoke-WebRequest $url -OutFile "$($Dest_folder)/$($File_name)"
+    Invoke-WebRequest $url -OutFile "./packs/$($File_name)"
 }
 
 
